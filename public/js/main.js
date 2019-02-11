4bc6a4b0a08b875a35eab356a1ee73de13fa0f0b
SungJe Kim <sungje.kim@one-line.com>
2019-02-11 10:58:14 +0900

    API 가져오기

---
+++ public/js/main.js
@@ -0,0 +1,13 @@
+// e1a342dc3fe3e3603e1ad544f3cdb8dd : Open Weather Map API keys
+
+// samples. 을 api.으로
+
+// #1 - Current Weather
+// https://api.openweathermap.org/data/2.5/weather?id=1835848&appid=e1a342dc3fe3e3603e1ad544f3cdb8dd&units=metric
+// #2 - 5 Days Weather & 3 Hours interval
+// https://api.openweathermap.org/data/2.5/forecast?id=1835848&appid=e1a342dc3fe3e3603e1ad544f3cdb8dd&units=metric
+
+// GMT +09 해줘야함. 영국 시간 기준이기 때문에
+
+
+
